efa66b2bb51286b71cae8ad201f4eb7cd186b2eb
AnhLe3698 <anhviet.le00@hotmail.com>
2022-11-04 19:15:40 -0600

    Add new button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,15 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+   let onClickEv = props.onClick ? props.onClick : ''
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+   buttonClass += " button--danger"
+  }
+
+  return <button onClick={onClickEv} className={buttonClass} disabled={props.disabled}>{props.children}</button>;
 }
