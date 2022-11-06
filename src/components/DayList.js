505543fdd486ee7d5c1411455758cb8e0410af79
AnhLe3698 <anhviet.le00@hotmail.com>
2022-11-06 11:18:54 -0700

    add initial day props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,11 +1,17 @@
-// Each day needs to display slightly differently depending on what state it is in. It should display the name of the day, whether it is selected or unselected, and the interview spots remaining. The user must also be able to select a particular day to view the interview information for that day. To put this all together we will need the following props:
+import React from "react";
+import DayListItem from "./DayListItem";
 
-// name:String the name of the day
-// spots:Number the number of spots remaining
-// selected:Boolean true or false declaring that this day is selected
-// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
-// We use the spots prop for two purposes:
+export default function DayList(props) {
 
-// 1) To display the text "{props.spots} spots remaining" 2) to determine if the day is full.
-
-// The DayListItem "knows" what it means to be full (if props.spots is 0, the day is full) but not what it means to be selected. It uses this prop directly to determine which styles to apply.
+  return (<ul>
+    {props.days.map((day) => {
+      return (<DayListItem
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props['day']}
+        setDay={props.setDay}
+      />);
+    })}
+  </ul>);
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,20 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   const classNamesEl = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': (props.spots === 0 ? true:false)})
+
+  function formatSpots(spots) {
+    if(spots===0) {
+      return 'no spots remaining';
+    } else if (spots===1) {
+      return '1 spot remaining';
+    } else {
+      return spots + ' spots remaining';
+    }
+  }
   return (
     <li onClick={() => {props.setDay(props.name)}} className={classNamesEl}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light" >{props.spots} spots remaining</h3>
+      <h3 className="text--light" >{formatSpots(props.spots)}</h3>
     </li>
   );
 }
