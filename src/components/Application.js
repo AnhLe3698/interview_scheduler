c9f0c44285e6c8d43ad8e36e5b7ba1b84d505c0b
AnhLe3698 <anhviet.le00@hotmail.com>
2022-11-15 04:29:43 -0700

    Adding use effect hooks and Axios get commands

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./appointment";
@@ -45,23 +45,15 @@ const appointments = {
 
 export default function Application(props) {
   let [dayCurr, setDayCurr] = useState('Monday');
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((manydays) => {
+      console.log(manydays.data)
+      setDays(manydays.data);
+    })
+  }, [])
+  
 
   return (
     <main className="layout">
